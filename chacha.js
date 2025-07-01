9f6893b8bda2d7d7d33cee66b438127a1091c3aa
kashyap1-GITHUB <baghelkashyap2004@gmail.com>
2025-07-02 01:54:34 +0530

    test server

---
+++ chacha.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+
+const server = http.createServer((req,res) => {
+    res.end("hello world chach from the server")    
+})
+
+
+
+
+server.listen(3000,() => {
+    console.log("server is starting now");
+})
