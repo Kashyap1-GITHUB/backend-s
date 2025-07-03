7588b7ba909ba1b4cac252d984a1abd3f8fad606
kashyap1-GITHUB <baghelkashyap2004@gmail.com>
2025-07-03 13:11:55 +0530

    how to use express

--- chacha.js
+++ chacha.js
@@ -1,13 +1,9 @@
-const http = require('http');
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  res.end("hello world chach from the server");
+});
 
-const server = http.createServer((req,res) => {
-    res.end("hello world chach from the server")    
-})
-
-
-
-
-server.listen(3000,() => {
-    console.log("server is starting now");
-})
+server.listen(3000, () => {
+  console.log("server is starting now");
+});
