20909ae3f15bb88eceed33a276d7c36ef9b32e0e
kashyap1-GITHUB <baghelkashyap2004@gmail.com>
2025-07-04 03:05:33 +0530

    how get request from frantend and how to send respons frantend

---
+++ day117.js
@@ -0,0 +1,24 @@
+const express = require('express'); // ✅ Use 'express' here
+
+const app = express(); // ✅ Correct way to create app
+
+app.use(express.json()); // ✅ Now this will work
+
+
+const arr = []
+
+app.post('/notes', (req, res) => {
+    
+    console.log(req.body); // ✅ This will show your JSON data
+
+    arr.push(req.body)
+
+   res.json({
+    message:"Notes created",
+    notes:arr     //send notes array format
+   })
+});
+
+app.listen(3000, () => {
+    console.log('Server is started on http://localhost:3000');
+});
